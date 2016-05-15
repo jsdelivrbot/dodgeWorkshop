d720bbe6612edffa7fc0fc40698f67ddd0e57f3b
Stanley <stanleyarmstrong31@gmail.com>
2016-05-15 05:52:48 +0000

    Initial commit

--- dodge.js
+++ dodge.js
@@ -1,11 +1,26 @@
 var player;
+var enemy ; 
 function setup(){
     createCanvas(250,250);
     player = createSprite(width/2,height,50,50);
+    enemy = createSprite(width/2 , 0 , 10 , 30); 
+    
     
 }
 function draw(){
-    drawBackground(0,100,0);
+    background(0,100,0);
+    
+    
+    
+    if (keyDown(RIGHT_ARROW) && player.position.x < (width = 25)){
+        player = player.position.x + 1;
+        
+    }if (keyDown(LEFT_ARROW) && player.position.x > 25){
+        player = player.position.x - 1; 
+        
+    }
+    enemy.position.y = enemy.position.y + 5;
+    
     drawSprites();
     
 }
